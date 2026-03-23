00d68b4ebb44581cdfab85b47ce82b6b4cef68eb
saucybooka <bhidealex687@gmail.com>
2026-03-23 10:04:26 -0700

    price comparator half done

--- PriceComparator.java
+++ PriceComparator.java
@@ -5,5 +5,26 @@ public class PriceComparator implements java.util.Comparator<TradeOrder>
 {
 
     // TODO complete class
+    public int compareOrders(TradeOrder order1, TradeOrder order2)
+    {
+        ; // not done, what is an ascending / descending comparator??? 
+        
+
+        if ((order1.isMarket() == true) && (order2.isMarket() == true))
+        {
+            return 0; 
+        }
+        if (order1.isMarket() == true)
+        {
+            return -1; 
+        }
+
+        if (order2.isMarket() == true)
+        {
+            return 1; 
+        }
+
+        double orderDifference = order1.getPrice() - order2.getPrice(); 
+    }
 
 }
