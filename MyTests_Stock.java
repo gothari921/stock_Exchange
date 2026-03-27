459ffbd84613c53720310b40a27f7ffce97a4e32
saucybooka <bhidealex687@gmail.com>
2026-03-27 08:20:12 -0700

    fulknado

--- MyTests_Stock.java
+++ MyTests_Stock.java
@@ -10,9 +10,11 @@ public class MyTests_Stock {
     {
         System.out.println("\n===== Stock Tests =====");
         testStockConstructor();
+        test2();
     }
 
-    public static void testStockConstructor() {
+    public static void testStockConstructor() 
+    {
         Stock stock = new Stock(symbol, companyName, initPrice);
         System.out.println(stock.toString());
         System.out.println("what the symbol should be: " + symbol);
@@ -25,9 +27,31 @@ public class MyTests_Stock {
         System.out.println("what the high price is:   " + stock.getHiPrice());
         System.out.println("what the last price should be: " + initPrice);
         System.out.println("what the last price is:   " + stock.getLastPrice());
-        System.out.println("Expected volume: 0");
         System.out.println("Actual volume:" + stock.getVolume());
     }
+
+    public static void test2()
+    {
+        Stock s = new Stock(symbol, companyName, initPrice);
+
+        System.out.println("Expected: contains 'Ask: none' and 'Bid: none'");
+        String quote = s.getQuote();
+        System.out.println("Actual: " + quote);
+    }
+
+    public static void test3()
+    {
+        Stock stock = new Stock(symbol, companyName, initPrice);
+        Trader trader = new Trader(null, "trader", "password" );
+        TradeOrder sellLimit = new TradeOrder(trader, symbol, false, true, 10, 10.00);
+        stock.placeOrder(sellLimit);
+        String quote = stock.getQuote();
+        System.out.println("Really: Ask contains 10.00 and size 10");
+        System.out.println("What we have" + quote);
+
+    }
+
+
 
     
 
